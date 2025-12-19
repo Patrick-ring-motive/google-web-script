f5163f995e764533f2ea0157522361ae71106dac
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-12-19 17:39:28 -0600

    Update web.js

--- web.js
+++ web.js
@@ -1,1 +1,47 @@
-class Web{}
+class Web{};
+(()=>{
+
+const Web = Web;
+
+const setProperty = (object,property)=>{
+  const [key,value] = Object.entries(property).pop()
+  return Object.definePropery(object,key,{
+    value,
+    enumerable:true,
+    configurable:false,
+    writeable:false
+  });
+};
+
+
+const Blob = class WebBlob extends Utilities.newBlob{
+
+  get size(){
+    return this.getBytes().length;
+  }
+
+  get type(){
+    return this.getContentType();
+  }
+
+  text(){
+    return this.getDataAsString();
+  }
+
+  bytes(){
+    return new Uint8Array(this.getBytes());
+  }
+
+  arrayBuffer(){
+    return new Uint8Array(this.getBytes()).buffer;
+  }
+
+  slice(){
+    return new Web.Blob(this.getBytes().slice(...arguments),this.getContentType());
+  }
+
+};
+
+setProperty(Web,{Blob});
+
+})();
